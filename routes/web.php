6383e079268b6eb434c43bc9ac4c2c2f2c8d4806
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-02-28 16:37:17 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome');,
     
        get('/', function () {
     return view('todos');
