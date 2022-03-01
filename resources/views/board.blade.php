27efa606baf127bcfeb968f7c75f913a2d9b3538
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-02-28 19:56:38 -0800

    Update board.blade.php

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -1,9 +1,9 @@
 @extends('adminlte::page')
 
-@section('title', 'To Do List')
+@section('title', 'Board')
 
 @section('content_header')
-    <h1>To Do's</h1>
+    <h1>Board</h1>
 @stop
 
 @section('content')
@@ -12,21 +12,25 @@
     <table id="table" class="table table-bordered">
       <thead>
         <tr>
-          <th style="width: 10px">#</th><th>Task</th><th>Progress</th><th style="width: 40px">Label</th>
+          <th style="width: 10px">#</th><th>Backlog</th><th>Create Labels</th><th style="width: 40px">Label</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td>1.</td>
-          <td>Update software</td>
-          <td><div class="progress progress-xs"><div class="progress-bar progress-bar-danger" style="width: 55%"></div></div></td>
-          <td><span class="badge bg-danger">55%</span></td>
+          <td>3.</td>
+          <td>Bug</td>
+          <td>Feature</td>
+          <td>Enchancement</td>
+          <td>Documentation</td>
+          <td>Examples</td>
+          //<td><div class="progress progress-xs"><div class="progress-bar progress-bar-danger" style="width: 55%"></div></div></td>
+          //<td><span class="badge bg-danger">55%</span></td>
         </tr>
         <tr>
           <td>2.</td>
-          <td>Clean database</td>
-          <td><div class="progress progress-xs"><div class="progress-bar bg-warning" style="width: 70%"></div></div></td>
-          <td><span class="badge bg-warning">70%</span></td>
+          <td>Create Issue Template</td>
+          //<td><div class="progress progress-xs"><div class="progress-bar bg-warning" style="width: 70%"></div></div></td>
+          //<td><span class="badge bg-warning">70%</span></td>
         </tr>
       </tbody>
     </table>
