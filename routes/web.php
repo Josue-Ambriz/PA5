486e15495a403a50a0be320009b90db00cffbcb1
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-02-28 18:51:13 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {return view('welcome');});
 Route::get('todos', function () {return view('todos');});
+Route::get('calender', function () {return view('calender');});
+Route::get('board', function () {return view('board');});
