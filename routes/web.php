3f94900cb98a365eb322d9861bb4ef997351daae
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-02 18:42:49 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 URL::forceScheme('https');
+
+Route::get('/db-test', function () { try{\DB::connection()->getPDO();
+                                          $db_name = \DB::connection()->getDatabaseName();
+                                          echo 'Database Connected: ' .$db_name;
+                                        } catch (\Exception $e) {echo 'None':}
+
 Route::get('/', function () {return view('welcome');});
 Route::get('/todos', function () {return view('todos');});
 Route::get('/calendar', function () {return view('calendar');});
