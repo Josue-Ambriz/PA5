adbf8f119888d554da3b25fedde335a79c651ab0
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-02 17:59:53 -0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Iluminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
