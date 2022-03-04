f171d2ec35ddc149fb41368ffbfa6c27ba07999f
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-03 20:17:43 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,15 @@ use Illuminate\Support\Facades\Route;
 
 URL::forceScheme('https');
 
-Route::get('/db-test', function () { try{\DB::connection()->getPDO();
-                                          $db_name = \DB::connection()->getDatabaseName();
-                                          echo 'Database Connected: ' .$db_name;
-                                        } catch (\Exception $e) {echo 'None':}
-                                    });
+Route::get('/db-test', function () {try{\DB::connection()->getPDO(); $db_name = \DB::connection()->getDatabaseName(); echo 'Database Connected: '.$db_name;} catch (\Exception $e) {echo 'None':}});
+Route::get('/db-migrate', function () {Artisan:call('migrate'); echo Artisan::output();});
 Route::get('/', function () {return view('welcome');});
 Route::get('/todos', function () {return view('todos');});
 Route::get('/calendar', function () {return view('calendar');});
-Route::get('/event-feed', function () {$path = storage_path()."/json/events.json"; return json_decode(file_get_contents($path), true);});
+
+//Route::get('/event-feed', function () {$path = storage_path()."/json/events.json"; return json_decode(file_get_contents($path), true);});
+
+
+
 Route::get('/board', function () {return view('board');});
 Route::fallback(function () {return view('error');});
