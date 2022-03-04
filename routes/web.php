add160ae7332e273ee1ca0a8b12dc80a21ecbe80
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-03 20:27:58 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/db-migrate', function () {Artisan:call('migrate'); echo Artisan::ou
 Route::get('/', function () {return view('welcome');});
 Route::get('/todos', function () {return view('todos');});
 Route::get('/calendar', function () {return view('calendar');});
-Route::get('/event-feed', function () {echo json_encode(array(array("title": "CSE4500 Class", "start": "2022-02-23T17:30:00", "end": "2022-02-23T18:45:00", array("title": "CSE4500 Class", "start": "2022-02-28T17:30:00", "end": "2022-02-28T18:45:00")));});
+Route::get('/event-feed', function () {echo json_encode(array(array("title" => "CSE4500 Class", "start" => "2022-02-23T17:30:00", "end" => "2022-02-23T18:45:00", array("title" => "CSE4500 Class", "start" => "2022-02-28T17:30:00", "end" => "2022-02-28T18:45:00")));});
 Route::get('/board', function () {return view('board');});
 Route::fallback(function () {return view('error');});
