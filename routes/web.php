dea3667f49ba3ddde57e90080d6f09847c7e95ba
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-03 18:50:42 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/db-test', function () { try{\DB::connection()->getPDO();
                                           $db_name = \DB::connection()->getDatabaseName();
                                           echo 'Database Connected: ' .$db_name;
                                         } catch (\Exception $e) {echo 'None':}
-                                   });
+                                    });
 Route::get('/home', function () {return view('welcome');});
 Route::get('/todos', function () {return view('todos');});
 Route::get('/calendar', function () {return view('calendar');});
