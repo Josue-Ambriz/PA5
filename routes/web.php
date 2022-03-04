a4daec4a5f85ad2ca8e820d56fb37faee9a74277
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-03 20:23:31 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 URL::forceScheme('https');
-
 Route::get('/db-test', function () {try{\DB::connection()->getPDO(); $db_name = \DB::connection()->getDatabaseName(); echo 'Database Connected: '.$db_name;} catch (\Exception $e) {echo 'None':}});
 Route::get('/db-migrate', function () {Artisan:call('migrate'); echo Artisan::output();});
 Route::get('/', function () {return view('welcome');});
 Route::get('/todos', function () {return view('todos');});
 Route::get('/calendar', function () {return view('calendar');});
-
 Route::get('/event-feed', function () {echo json_encode(array(array("title": "CSE4500 Class", "start": "2022-02-23T17:30:00", "end": "2022-02-23T18:45:00", array("title": "CSE4500 Class", "start": "2022-02-28T17:30:00", "end": "2022-02-28T18:45:00")));});
-
-
-
 Route::get('/board', function () {return view('board');});
 Route::fallback(function () {return view('error');});
