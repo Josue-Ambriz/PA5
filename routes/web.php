55925581b745c1b57e74609c0fbf5979b2e5f878
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-07 18:39:48 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,13 @@ use Illuminate\Support\Facades\Route;
 
 URL::forceScheme('https');
 
-/*Route::get('/db-test', function () {
+Route::get('/db-test', function () {
     try {         
          echo \DB::connection()->getDatabaseName();     
     } catch (\Exception $e) {
           echo 'None';
     }
-});*/
+});
 
 Route::get('/db-test', function () {try{\DB::connection()->getPDO(); $db_name = \DB::connection()->getDatabaseName(); echo 'Database Connected: '.$db_name;} catch (\Exception $e) {echo 'None';}});
 Route::get('/db-migrate', function () {Artisan:call('migrate'); echo Artisan::output();});
