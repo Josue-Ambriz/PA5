253b39552dfe5e63518694af025399ccf8c9c9bb
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-07 17:52:31 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -36,10 +36,10 @@ Route::get('/events-feed', function () {
             'start' => '2022-02-23T17:30:00',
             'end' => '2022-02-23T18:45:00'
           ),
-        array(
-      'title' => 'CSE4500 Class',
-      'start' => '2022-02-28T17:30:00',
-      'end' => '2022-02-28T18:45:00'
+      array(
+            'title' => 'CSE4500 Class',
+            'start' => '2022-02-28T17:30:00',
+            'end' => '2022-02-28T18:45:00'
           ),
         );
         return json_encode($data);
