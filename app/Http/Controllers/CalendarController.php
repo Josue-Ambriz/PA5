45996eb6998d932c72c204f3ba5b2bb342a01cd7
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-10 16:59:04 -0800

    Update CalendarController.php

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Todo;
 
-class TodoController extends Controller
+class CalendarController extends Controller
 {
     /**
      * Display a listing of the resource.
