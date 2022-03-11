d904742aada6d9947397426e9977680327a57945
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-11 13:59:58 -0800

    Update CalendarEvent.php

--- app/Models/CalendarEvent.php
+++ app/Models/CalendarEvent.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Calendar extends Model
+class CalendarEvent extends Model
 {
     use HasFactory;
-    protected $table = 'calendarevent';
-    protected $fillable = ['title', 'begin', 'finish'];
+    //protected $table = 'calendarevent';
+    protected $fillable = ['title', 'starting', 'ending'];
 }
