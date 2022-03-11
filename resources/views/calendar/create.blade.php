030ae8ef6ed1a57f59211934cbd475f52b4a7ada
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-10 20:51:05 -0800

    Update create.blade.php

--- resources/views/calendar/create.blade.php
+++ resources/views/calendar/create.blade.php
@@ -10,6 +10,7 @@
 <form method="post" action="{{ route('calendar.store') }}" >
     @csrf
     <x-adminlte-input name="title" label="Title" />
+    <x-adminlte-input name="offset" id="hiddenoffsetinput" type="hidden" />
     <x-adminlte-input name="begin" type="datetime-local" label="Start" />
     <x-adminlte-input name="finish" type="datetime-local" label="Finish" />
     <x-adminlte-button type="Submit" label="Submit" />
