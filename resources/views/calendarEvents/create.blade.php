a83e475e730ef134df95839be1c1f8804ba3a71f
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-11 15:26:09 -0800

    Create create.blade.php

---
+++ resources/views/calendarEvents/create.blade.php
@@ -0,0 +1,17 @@
+@extends('adminlte::page')
+
+@section('title', 'Calendar Event')
+
+@section('content_header')
+    <h1>Add an Event</h1>
+@stop
+
+@section('content')
+<form method="post" action="{{ route('calendar-events.store') }}" >
+    @csrf
+    <x-adminlte-input name="title" label="Title" />
+    <x-adminlte-input name="start_at" type="datetime-local" label="Start" />
+    <x-adminlte-input name="end_at" type="datetime-local" label="End" />
+    <x-adminlte-button type="Submit" label="Submit" />
+</form>
+@stop
