1a783c9b4e811aacd4b332613c65194ab18185e4
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-10 17:44:32 -0800

    Update CalendarController.php

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Todo;
+use App\Models\Calendar;
 
 class CalendarController extends Controller
 {
@@ -14,7 +14,7 @@ class CalendarController extends Controller
      */
     public function index()
     {
-        $events = Event::select('title', 'start_at AS start', 'end_at AS end')->get();
+        $events = Calendar::select('title', 'start_at AS start', 'end_at AS end')->get();
         return view('calendar', compact('events')); 
     }
 
