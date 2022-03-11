a1e0a759d1d51da9b52d5a6e1d381005f5a1d3c9
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-11 14:12:23 -0800

    Update create.blade.php

--- resources/views/calendar/create.blade.php
+++ resources/views/calendar/create.blade.php
@@ -11,7 +11,7 @@
     @csrf
     <x-adminlte-input name="title" label="Title" />
     <x-adminlte-input name="starting" type="datetime-local" label="Start" />
-    <x-adminlte-input name="ending" type="datetime-local" label="Finish" />
+    <x-adminlte-input name="ending" type="datetime-local" label="End" />
     <x-adminlte-button type="Submit" label="Submit" />
 </form>
 @stop
