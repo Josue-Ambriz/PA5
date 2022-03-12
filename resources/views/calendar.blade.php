129008ea2711215e4a082b3103893656bc33c194
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-11 18:01:35 -0800

    Update calendar.blade.php

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -4,6 +4,7 @@
 
 @section('content_header')
     <h1>Calendar</h1>
+    <a href="{{ route('calendar.create') }} " class="btn btn-primary" >Create</a>
 @stop
 
 @section('content')
@@ -17,7 +18,6 @@
     </div>
   </div>
 </div>
-<a href="{{ route('calendarEvents.create') }} " class="btn btn-primary" >Create</a>
 @stop
 
 @section('css')
