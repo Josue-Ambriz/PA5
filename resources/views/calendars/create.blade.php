6abe246e7fd6fd52ad590ebf32fe03516ed8c9a6
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-11 17:56:03 -0800

    Update create.blade.php

--- resources/views/calendars/create.blade.php
+++ resources/views/calendars/create.blade.php
@@ -10,9 +10,8 @@
 <form method="post" action="{{ route('calendar.store') }}" >
     @csrf
     <x-adminlte-input name="title" label="Title" />
-    <x-adminlte-input name="startTime" type="datetime-local" label="Start" />
-    <x-adminlte-input name="endTime" type="datetime-local" label="End" />
-
+    <x-adminlte-input name="starting" type="datetime-local" label="Start" />
+    <x-adminlte-input name="ending" type="datetime-local" label="End" />
     <x-adminlte-button type="Submit" label="Submit" />
 </form>
 @stop
